f89778151ba26e6d235fdb8a6e137dd166edef6b
yaojie <1750724954@qq.com>
2023-11-09 23:10:41 +0800

    初始化

--- testcases/denglu.py
+++ testcases/denglu.py
@@ -29,36 +29,28 @@ class Test_demo():
         register=requests.post(url, data=data, headers=head)
         register = json.loads(register.text)
         print(register)
-    # def test_demo5(self):
-    #     url = "https://www.wh00.ooo/api/email/login"
-    #
-    #     test = {
-    #         "Host": "www.wh00.ooo",
-    #         "Cookie": "_message_backend_session=X7plfcvk1nuSMznj4r2SejthiEXBMTocc9XUTB0zovc%2FzVVX63AqmqEpGIMC4m4ShNcaTGS%2BqMguPl2Bj%2FJ4%2FR%2FUEMnAyajSy1WR2Mz%2FMzkkLRnYFL7CH6OOHY6qwctoTCy5paxsbMTqed5fLfwJRQ126LIQVjG6ONbQ6HrH4KbhAuIMf%2BOmgwEcdA59P2TWcbP0T8MSoKX1UdPXXqN6gj1vFY8Rr9Sm7qWaegsJZM%2Bf0WyB--xV6Wugdt0cl9csp7--3V9FwaNiYBeb5PZfvlIptA%3D%3D",
-    #         "accept": "application/json",
-    #         "user-agent": "app.whoo/0.14.4 iOS/15.8",
-    #         "accept-language": "zh-JP"
-    #     }
-    #     data = {
-    #         "email": "1420990591@qq.com",
-    #         "password": "12345678"
-    #     }
-    #     List = requests.post(url, data=data, headers=test)
-    #     List = json.loads(List.text)
-    #     print(List)
 
-    # def test_xiugai(self):
-    #     url= 'https://www.wh00.ooo/api/user?user'
-    #     test = {'Host': "www.whoo.ooo",
-    #             'Cookie': '_message_backend_session=IC6bdBMXV3WCFQV3FaFkGPoMpIiBY3mnw1Ulug0IMVll%2B1sNsj3ysPTNb%2F0hH39x1OC%2BBRe%2BKAPD1MsaumRFtqmWsxLr%2F%2BBMo2P7I0nCIUsME7coNQx%2B0BDhqxFfFwtSvVGA4bZ%2FOhwaX%2F3hKv0GCXWr6xZ%2FGwmuQmyoiI%2Bb2VqkBbJ1V67KPvm0hVVcKtP9KEJ8MM8%2Fn62uHlHq8Fu0M6sGX16h2TRSNLkMp8wwulHFi1FH--caKVZGSE3dUWKD%2BM--sQZVCKKgbXUCoKy%2B9rfUoQ%3D%3D',
-    #             "accept": "application/json",
-    #             'user-agent': 'app.whoo/0.14.3 iOS/16.3',
-    #             'accept-language':'zh-CN',
-    #             'authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMjUyMjA5OX0.aIQ1ALNFyGrXg_Iu4xX54hrORksbdjYCXteuim0F7xU'}
-    #     data= {'user[username]': 'zhangyaojie'}
-    #     xiugai=requests.patch(url, data=data,headers=test )
-    #     print(xiugai.status_code)
-    #     print(xiugai.content)
+    def test_demo5(self):
+        url = "https://www.wh00.ooo/api/user"
+
+        test = {
+            "Host": "www.wh00.ooo",
+            "accept": "application/json",
+            "user-agent": "app.whoo/0.14.3 iOS/16.3",
+            "accept-language": "zh-CN",
+            'authorization' : 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMjUyMjA5OX0.aIQ1ALNFyGrXg_Iu4xX54hrORksbdjYCXteuim0F7xU',
+            'Accept-Encoding' : 'gzip, deflate, br',
+            'Connection' : 'keep-alive',
+            'Cookie' : '_message_backend_session=AYLtZmMX4mKXsTOS1hA7L8QFQPOpse2iHTQPrAg5HTdO%2FB9IS8LHAoK8lTir%2FCS3jz7QnEJcwxYtoeEAK81ZZINxSRmBnx%2FnuWyhiRPf4J028TuokVh1R3GId43pIjxO3cwoIBbLHUP260pEcbBtWZpHbd43YtRnTfUnk8n1j22wBUA9jEMVk%2FrXlg0%2BRLVrhq4fkRsUVomA87zAjjS%2Bar2bohpZ5ZDzrfT5D%2B5JLgi9idJA--bRl5USpWJ93Wve4D--%2FU1yHKQSn0uYdQkCJfGUqA%3D%3D',
+
+        }
+        data = {
+            'user[username]': '142099059432',
+            'user[display_name]' : 'llllllllllllll'
+        }
+        List = requests.patch(url, data=data, headers=test)
+        List = json.loads(List.text)
+        print(List)
 
 
 if __name__ == "__main__":
